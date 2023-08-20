3944ee2950003929efba815a791cfef553dd9e5e
sadagayathri <gayathrisada022@gmail.com>
2023-08-20 13:46:39 +0530

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,49 @@
 import './App.css'
 
 function App() {
+  const students=[
+    {
+      'firstname':'Gayathri',
+      'lastname':'Sada',
+      'gender':'female'
+    },
+    {
+      'firstname':'subbarao',
+      'lastname':'Sada',
+      'gender':'male'
+    },
+    {
+      'firstname':'srivani',
+      'lastname':'Sada',
+      'gender':'female'
+    },
+    {
+      'firstname':'surya',
+      'lastname':'Sada',
+      'gender':'male'
+    }
+  ];
   
 
   return (
     <>
-      <h1>welcome</h1>
-      <h1>hello</h1>
+      <table border={2}>
+   
+   <thead>
+     <th>firstname</th>
+     <th>Lastname</th>
+     <th>Gender</th>
+   </thead>
+   <tbody>
+    {
+       students.map((a)=>{
+           return  <tr><td>{a.firstname}</td><td>{a.lastname}</td><td>{a.gender}</td></tr>
+        
+     })
+    }
+    
+   </tbody>
+</table>
     </>
   )
 }
