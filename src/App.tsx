0e2fe7a5fa422934ac4dfc81c17e2d6f8d56d998
sadagayathri <gayathrisada022@gmail.com>
2023-08-21 15:17:59 +0530

    sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,74 @@
 
 import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 function App() {
-  
+
+  const [count,setcount] = React.useState([
+    {
+      firstname:"praveen",
+      lastname:"Gubbala",
+      age:40,
+      gender:"male"
+    },
+    {
+      firstname:"mrithi",
+      lastname:"mandana",
+      age:25,
+      gender:'female'
+    },
+    {
+      firstname:"dithali",
+      lastname:"raj",
+      age:39,
+      gender:'female'
+    },
+    {
+      firstname:"rohit",
+      lastname:"Sharma",
+      age:35,
+      gender:'male'
+    },
+    {
+      firstname:"sachin",
+      lastname:"Tendulkar",
+      age:50,
+      gender:'male'
+    },
+])
+function abc(){
+  setcount([...count].sort((b,c)=>{
+    return (c.firstname<b.firstname)?1:-1
+
+  }))
+}
 
   return (
     <>
-      <h1>welcome</h1>
-      <h1>hello</h1>
+      <table border={2}>
+   
+   <thead>
+     <th onClick={abc}>firstname</th>
+     <th>Lastname</th>
+     <th>Age</th>
+     <th>Gender</th>
+   </thead>
+   <tbody>
+    {
+       count.map((a)=>{
+           return  <tr>
+            <td>{a.firstname}</td>
+            <td>{a.lastname}</td>
+            <td>{a.age}</td>
+            <td>{a.gender}</td>
+            </tr>
+        
+     })
+    }
+    
+   </tbody>
+</table>
     </>
   )
 }
