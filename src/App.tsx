5e66e4856ca5ae389d413187ffe20ecb86a4a9f5
sadagayathri <gayathrisada022@gmail.com>
2023-08-24 12:21:06 +0530

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 
 import './App.css'
-
+import React from 'react'
 function App() {
-  
+  const [name,setName]=React.useState("")
+  const [phonenumber,setPhonemunber]=React.useState("")
+  const [email,setEmail]=React.useState("")
+  function save(){
+    window.localStorage.setItem("name",name)
+    window.localStorage.setItem("phonenumber",phonenumber)
+    window.localStorage.setItem("email",email)
+  }
 
   return (
     <>
-      <h1>welcome</h1>
-      <h1>hello</h1>
+      <input type="text" onChange={(event)=>{event.target.value}}/>
+      <br />
+      <input type="text" onChange={(event)=>{event.target.value}}/>
+      <br />
+      <input type="text" onChange={(event)=>{event.target.value}}/>
+      <br />
+      <button onClick={()=>{save}}>Save</button>
     </>
   )
 }
